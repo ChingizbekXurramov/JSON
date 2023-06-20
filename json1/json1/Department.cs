9a58337e7fc2a743a3fa2c06c4d6ff995d0ff9b9
Chingizbek <128830130+ChingizbekXurramov@users.noreply.github.com>
2023-06-20 22:06:06 +0500

    Json

---
+++ json1/json1/Department.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace json1
+{
+    internal class Department
+    {
+        public string DepartmentName { get; set; }
+        public List<Employee> Employees { get; set; }
+    }
+
+    public class Employee
+    {
+        public string EmployeeName { get; set; }
+    }
+}
---
+++ json1/json1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+namespace json1
+{
+    internal class Program
+    {
+    
+        static void Main(string[] args)
+        {
+            Department department = new Department
+            {
+                DepartmentName = "IT",
+                Employees = new List<Employee>
+                {
+                    new Employee { EmployeeName = "John Doe" },
+                    new Employee { EmployeeName = "John John" }
+                }
+            };
+
+            string json = JsonConverter.SerializeObject(department);
+            File.WriteAllText("department.json", json);
+            string jsonFromFile = File.ReadAllText("department.json");
+            Department deserializedDepartment = JsonConverter.DeserializeObject<Department>(jsonFromFile);
+
+            Console.WriteLine($"Department Name: {deserializedDepartment.DepartmentName}");
+            Console.WriteLine("Employees:");
+            foreach (Employee employee in deserializedDepartment.Employees)
+            {
+                Console.WriteLine(employee.EmployeeName);
+            }
+        }
+    }
+}
